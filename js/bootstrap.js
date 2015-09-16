d4b4b4f9acf2f4c22dac5a83a1a8efede480ae51
Kamil <kjankowski85@gmail.com>
2015-09-16 21:52:42 +0100

    top nav bar v1

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -4,6 +4,18 @@
  * Licensed under the MIT license
  */
 
+// Copyright 2014-2015 Twitter, Inc.
+// Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+  var msViewportStyle = document.createElement('style')
+  msViewportStyle.appendChild(
+    document.createTextNode(
+      '@-ms-viewport{width:auto!important}'
+    )
+  )
+  document.querySelector('head').appendChild(msViewportStyle)
+}
+
 if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
 }
